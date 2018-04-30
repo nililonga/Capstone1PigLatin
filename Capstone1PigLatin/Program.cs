5b456b0a71ca75bf043466e3cb88e70e03df3b07
noelle none <noe.ililonga@gmail.com>
2018-04-30 08:06:03 -0400

    fixed loop and bugs

--- Capstone1PigLatin/Program.cs
+++ Capstone1PigLatin/Program.cs
@@ -9,56 +9,64 @@ namespace Capstone1PigLatin
     class Program
     {
         static void Main(string[] args)
+
+        
         {
-
-            string data;
-            string PigEnd;
-            string start;
-            string data2;
-            string vowels = "aeiou";
-
-
-            //Prompt user to enter word
-            Console.WriteLine("Enter a word into the Pig Latin Converter!");
-
-            //convert word to "data"
-            data = (Console.ReadLine().ToLower());
-
-            //provide string used for pig latin ending
-            PigEnd = "way";
-
-            //isolate end of way for consonants
-            start = PigEnd.Substring(1, 2);
-
-            //if data is vowel
-            if (data[0] == 'a' || data[0] == 'e' || data[0] == 'i' || data[0] == 'o' || data[0] == 'u')
+            int y;
+            string answer1 = "y";
+            do
             {
-                Console.WriteLine("Your new word is:" + data + PigEnd);
-            }
-            else
-            {
+                //declare
+                string data;
+                string PigEnd;
+                string start;
+                string vowels = "aeiou";
+                
+                //Prompt user to enter word
+                Console.WriteLine("Enter a word into the Pig Latin Converter!");
 
-                for (int i = 0; i < data.Length; i++)
+                //convert word to "data"
+                data = (Console.ReadLine().ToLower());
+
+                //provide string used for pig latin ending
+                PigEnd = "way";
+
+                //isolate end of way for consonants
+                start = PigEnd.Substring(1, 2);
+
+                //if data is vowel
+                if (data[0] == 'a' || data[0] == 'e' || data[0] == 'i' || data[0] == 'o' || data[0] == 'u')
                 {
-                    for (int j = 0; j < vowels.Length; j++)
+                    Console.WriteLine("Your new word is: " + data + PigEnd);
+                    Console.Write("Would you like to continue? (y/n):  ");
+                    answer1 = Console.ReadLine();
+                }
+                else
+                {
+                    //locate/isolate consonants in word
+                    for (int i = 0; i < data.Length; i++)
                     {
-
-                        if (data[i] == vowels[j])
+                        //isolate
+                        for (int j = 0; j < vowels.Length; j++)
                         {
-                            string end = data.Substring(i, data.Length -i);
-                            string start1 = data.Substring(0,i);
+                            if (data[i] == vowels[j])
+                            {
+                                //determine where to move
+                                string end = data.Substring(i, data.Length - i);
+                                string start1 = data.Substring(0, i);
 
-                            Console.WriteLine(end + start1 + "ay");
-                            j = vowels.Length;
-                            i = data.Length;
+                                Console.WriteLine("Your new word is: " + end + start1 + "ay");
+                                j = vowels.Length;
+                                i = data.Length;
+                            }
                         }
                     }
+                          Console.Write("Would you like to continue? (y/n):  ");
+                         answer1 = Console.ReadLine();
+                    } 
+              
+            } while (answer1 == "y");
 
-                }
-
-
-    
-            }
         }
     }
 }
